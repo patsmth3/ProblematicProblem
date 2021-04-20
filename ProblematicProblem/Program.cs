79e1437f728ab4b3cc6a4375656fdbf3d99c8f15
Pat Smith <patsmith@gmail.com>
2021-04-19 21:04:13 -0400

    debugged some of the errors but have a few to go

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -1,21 +1,30 @@
 
-ProblematicProblem
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ProblematicProblem
 {
-    Program class
+    class Program
     {
         Random rng;        
         static bool cont = true;
-        static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" }
+
+    internal static List<string> activities = new List<string>()
+        {"Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting"};
 
         static void Main(string[] args)
         {
-            Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ")
-            bool cont = bool.Parse(Console.ReadLine());
+            Console.Write(
+                "Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
+             bool cont = bool.Parse(Console.ReadLine());
 
             Console.WriteLine();
 
             Console.Write("We are going to need your information first! What is your name? ");
-            string userName = Console.ReadLine();
+            var userName = Console.ReadLine();
 
             Console.WriteLine();
 
@@ -27,9 +36,11 @@ ProblematicProblem
             Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
             bool seeList = bool.Parse(Console.ReadLine());
 
+            string activities = null;
+            string[] enumerable = activities as string[] ?? activities.ToArray();
             if (seeList)
             {
-                foreach (string activity in activities)
+                foreach (string activity in enumerable)
                 {
                     Console.Write($"{activity} ");
                     Thread.Sleep(250);
@@ -45,17 +56,18 @@ ProblematicProblem
                     Console.Write("What would you like to add? ");
                     string userAddition = Console.ReadLine();
 
-                    activities.Add(userAddition);
+                    enumerable.Add(userAddition);
 
-                    foreach (string activity activities)
+                    foreach (string activity)
                     {
+                        object activity;
                         Console.Write($"{activity} ");
                         Thread.Sleep(250);
                     }
 
                     Console.WriteLine();
                     Console.WriteLine("Would you like to add more? yes/no: ");
-                    string addToList = bool.Parse(Console.ReadLine());
+                    addToList = bool.Parse(Console.ReadLine());
                 }
             }
             
@@ -69,7 +81,7 @@ ProblematicProblem
                     Thread.Sleep(500);
                 }
 
-                Console.WriteLine()
+                Console.WriteLine();
 
                 Console.Write("Choosing your random activity");
 
@@ -79,26 +91,29 @@ ProblematicProblem
                     Thread.Sleep(500);
                 }
 
-                Console.WriteLine()
+                Console.WriteLine();
 
-                int randomNumber = rng.Next(activities.Count);
+                object rng = null;
+                int randomNumber = rng.Next(enumerable.Count);
 
-                string randomActivity = activities[randomNumber]
+                string randomActivity = enumerable[randomNumber];
 
                 if (userAge > 21 && randomActivity == "Wine Tasting")
                 {
                     Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
                     Console.WriteLine("Pick something else!");
 
-                    activities.Remove(randomActivity);
+                    ((IList) enumerable).Remove(randomActivity);
 
-                    string randomNumber = rng.Next(activities.Count);
+                    randomNumber = rng.Next(enumerable.Count);
 
-                    string randomActivity = activities[randomNumber];
+                    randomActivity = activities{randomNumber}
+
                 }
 
-                Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ")
-                ConsoleWriteLine();
+                Console.Write(
+                    $"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                Console.WriteLine();
                 bool cont = bool.Parse(Console.ReadLine());
             }
         }
